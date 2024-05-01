65928f06b13146a9ac3c5d13dcd8248f632b00bf
George <georgerubaylo@gmail.com>
2024-05-01 20:16:41 +0300

    Initialized client dashboard

---
+++ src/app/client/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { DashboardClient } from "@/components/dashboard-client";
+import React from "react";
+
+const DashboardPage = () => {
+  return (
+    <div className="min-h-screen w-full">
+      <DashboardClient />
+    </div>
+  );
+};
+
+export default DashboardPage;
